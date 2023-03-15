8f622572540db9392911bd5cb35ca270741c2e05
Emanuel José <emanuel.jsa21@gmail.com>
2023-03-15 11:28:13 -0300

    Adding xylophone and audio functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SafeArea(
-        child: Placeholder(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                keyNote(0xffD00000, 1),
+                keyNote(0xffFB5607, 2),
+                keyNote(0xffFFBE0B, 3),
+                keyNote(0xffFF006E, 4),
+                keyNote(0xff8338EC, 5),
+                keyNote(0xff3A86FF, 6),
+                keyNote(0xff7BFFD0, 7),
+              ],
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  playNote(int note) async {
+      AudioPlayer player = AudioPlayer();
+      await player.setSource(AssetSource('note$note.wav'));
+      await player.resume();
+  }
+
+  keyNote(int color, int note) {
+      return Expanded(
+        child: MaterialButton(
+          onPressed: () async { playNote(note); } ,
+          color: Color(color),
+        ),
+      );
   }
 }
